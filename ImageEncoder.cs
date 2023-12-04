6a534a618a89882995e5c15bd244c9c02cb913ae
Robert Kasper <roberthkasper@gmail.com>
2023-12-03 20:59:22 -0500

    Adds ImageEncoder.cs and removes Aspose.Imaging dependency

---
+++ ImageEncoder.cs
@@ -0,0 +1,11 @@
+﻿namespace BeatSaberLibraryManager;
+
+public class ImageEncoder
+{
+	public static string Base64Encode(string path) 
+	{
+		byte[] imageArray = System.IO.File.ReadAllBytes(path);
+		string base64ImageRepresentation = Convert.ToBase64String(imageArray);
+		return base64ImageRepresentation;
+	}
+}
