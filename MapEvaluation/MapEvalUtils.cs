8f691069ba4cb2a4feafb216ed8857f0f4dd6c88
Robert Kasper <roberthkasper@gmail.com>
2023-12-03 21:09:05 -0500

    Adds Map evaluation code

---
+++ MapEvaluation/MapEvalUtils.cs
@@ -0,0 +1,26 @@
+﻿namespace BeatSaberLibraryManager.MapEvaluation
+{
+	public static class MapEvalUtils
+	{
+		public static double Remap(this double f, double inMin, double inMax, double outMin, double outMax)
+		{
+			double inPortion = Math.Clamp((f - inMin) / (inMax - inMin), 0, 1);
+			return outMin + inPortion * (outMax - outMin);
+		}
+		public static string FilterToJustAlphaNumerics(string str)
+		{
+			string result = "";
+			foreach (char c in str)
+			{
+				if (char.IsLetterOrDigit(c))
+					result += c;
+				else
+					result += ' ';
+			}
+
+			return result;
+		}
+
+		public static int FindWordsInMapName(this Doc doc, string[] words) => words.Count(w => doc.name.Contains(w, StringComparison.OrdinalIgnoreCase));
+	}
+}
---
+++ MapEvaluation/MapFilters.cs
@@ -0,0 +1,50 @@
+﻿using SpotifyAPI.Web;
+
+namespace BeatSaberLibraryManager.MapEvaluation
+{
+	public static class MapFilters
+	{
+		public static bool FailsAnyQualityFilter(this Doc d)
+		{
+			Version v = d.GetLatestVersion();
+			return d.IsPoorlyRated() || v.HasTooManyParityErrors() || v.NpsIsTooHigh();
+		}
+		
+		public static bool HasTooManyParityErrors(this Version v, int maxParityErrors = 5)
+		{
+			foreach (Diff diff in v.diffs)
+			{
+				if(diff.difficulty != Diff.Normal && diff.difficulty != Diff.Easy)
+					if (diff.paritySummary.errors > maxParityErrors)
+						return true;
+			}
+			return false;
+		}
+
+		public static bool NpsIsTooHigh(this Version version, float maxNps = 6.5f)
+		{
+			return version.diffs.All(d => d.nps > maxNps);
+		}
+
+		public static bool IsPoorlyRated(this Doc doc)
+		{
+			bool hasManyRatings = doc.stats.upvotes + doc.stats.downvotes > 50;
+
+			if (hasManyRatings)
+				return doc.stats.score < .8f;
+			else
+				return doc.stats.score < .7f;
+		}
+
+		public static bool HasAnyOfRequestedDifficulties(this Version v, string[] desiredDiffs) => v.diffs.Any(d => desiredDiffs.Any(desired => d.difficulty == desired));
+
+		public static bool ContainsArtistName(this Doc doc, FullTrack fullTrack)
+		{
+			string[] artistNameWords = MapEvalUtils.FilterToJustAlphaNumerics(fullTrack.Artists.First().Name).Split(' ');
+			int artistNameWordsFound = doc.FindWordsInMapName(artistNameWords);
+			bool containsArtistName = artistNameWordsFound > .7f * artistNameWords.Length;
+
+			return containsArtistName;
+		}
+	}
+}
