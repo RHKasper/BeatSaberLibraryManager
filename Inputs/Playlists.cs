1a4bedd491019b720cf6bf7c2456a1c20aecb22b
Robert Kasper <roberthkasper@gmail.com>
2023-12-09 18:39:46 -0500

    Adds BeatSaverSharper plugin from NuGet. Todo: remove custom models and API access.

--- Inputs/Playlists.cs
+++ Inputs/Playlists.cs
@@ -2,36 +2,42 @@
 
 public class Playlists
 {
-	public static readonly Dictionary<string, string> BpListUrls = new()
+	public static readonly Dictionary<string, int> BeatSaverMapperPlaylists = new()
 	{
-		// BeatSaver Mapper Playlists
-		{"Teuflum", GetBeatSaverMapperPlaylistUrl(68740)},
-		{"TheCzar1994", GetBeatSaverMapperPlaylistUrl(4285984)},
-		{"NixieKorten - Electroswing mapper ", GetBeatSaverMapperPlaylistUrl(4286374)},
-		{"Revelate", GetBeatSaverMapperPlaylistUrl(2768)},
-		
-		// BeastSaber Playlists
+		{ "Teuflum", 68740 },
+		{ "TheCzar1994", 4285984 },
+		{ "NixieKorten - Electroswing mapper ", 4286374 },
+		{ "Revelate", 2768 },
+	};
+
+	public static readonly Dictionary<string, int> BeatSaverPlaylists = new()
+	{
+		{ "Favorites", 7903 },
+		{ "Aspirational", 7038 },
+		{ "Ajr NeoTheater", 171573 },
+		{ "Skillet", 85217 },
+		{ "Linkin Park", 12144 },
+		{ "KDA", 70553 },
+		{ "BTS", 70543 },
+	};
+
+	public static readonly Dictionary<string, string> BeastSaberPlaylists = new()
+	{
 		{ "Alphabeat PixelTerror", "https://bsaber.com/PlaylistAPI/21-07-01_pixel-terror-pack_alphabeat.bplist" },
-		
-		// BeatSaver Playlists
-		{ "Favorites", GetBeatSaverPlaylistUrl(7903) },
-		{ "Aspirational", GetBeatSaverPlaylistUrl(7038) },
-		{ "Ajr NeoTheater", GetBeatSaverPlaylistUrl(171573) },
-		{ "Skillet", GetBeatSaverPlaylistUrl(85217) },
-		{ "Linkin Park", GetBeatSaverPlaylistUrl(12144) },
-		{ "KDA", GetBeatSaverPlaylistUrl(70553) },
-		{ "BTS", GetBeatSaverPlaylistUrl(70543) },
 	};
-        
-	public static readonly Dictionary<string,string> SpotifyPlaylistUrls = new()
+
+	public static readonly Dictionary<string, string> SpotifyPlaylistUrls = new()
 	{
-		{"Robert's Epic Mix", "https://open.spotify.com/playlist/6QTGBDOzjxnkfQ9X6OmWIY?si=9d82829900c84462"},
-		{"Robert's Pop Mix", "https://open.spotify.com/playlist/7vSZ2b4591qCZhi8bn2xkn?si=4a570e22013747fa"},
-		{"Robert's Edm Mix", "https://open.spotify.com/playlist/5tY88XWKaUArUISNkY697j?si=3afb235dac594189"},
-		{"Robert's Rock Mix", "https://open.spotify.com/playlist/43jMBPZVR5cdD7Cw1gZF8j?si=9a990d1647eb4cde"},
-		{"All Likes", "https://open.spotify.com/playlist/5Zi1NzMK91ImLODGEWHNqS?si=bcde847d6766403f"},
+		{ "Robert's Epic Mix", "https://open.spotify.com/playlist/6QTGBDOzjxnkfQ9X6OmWIY?si=9d82829900c84462" },
+		{ "Robert's Pop Mix", "https://open.spotify.com/playlist/7vSZ2b4591qCZhi8bn2xkn?si=4a570e22013747fa" },
+		{ "Robert's Edm Mix", "https://open.spotify.com/playlist/5tY88XWKaUArUISNkY697j?si=3afb235dac594189" },
+		{ "Robert's Rock Mix", "https://open.spotify.com/playlist/43jMBPZVR5cdD7Cw1gZF8j?si=9a990d1647eb4cde" },
+		{ "All Likes", "https://open.spotify.com/playlist/5Zi1NzMK91ImLODGEWHNqS?si=bcde847d6766403f" },
 	};
-	
-	private static string GetBeatSaverPlaylistUrl(int playlistId) => "https://api.beatsaver.com/playlists/id/" + playlistId + "/download";
-	private static string GetBeatSaverMapperPlaylistUrl(int mapperId) => "https://api.beatsaver.com/users/id/" + mapperId + "/playlist";
+
+	private static string GetBeatSaverPlaylistUrl(int playlistId) =>
+		"https://api.beatsaver.com/playlists/id/" + playlistId + "/download";
+
+	private static string GetBeatSaverMapperPlaylistUrl(int mapperId) =>
+		"https://api.beatsaver.com/users/id/" + mapperId + "/playlist";
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Diagnostics;
 using BeatSaberLibraryManager.Inputs;
 using BeatSaberLibraryManager.WebDownload;
+using BeatSaverSharp;
+using BeatSaverSharp.Models.Pages;
 
 namespace BeatSaberLibraryManager;
 
@@ -8,16 +10,26 @@ public class Program
 {
 	public static Task Main()
 	{
-		Stopwatch stopwatch = Stopwatch.StartNew();
-		List<Task<BPList>> tasks = new List<Task<BPList>>(Playlists.BpListUrls.Count + Playlists.SpotifyPlaylistUrls.Count);
 		
-		tasks.Add(HighLevelTasks.GetWebBpListAsync(Playlists.BpListUrls.Values.First()));
-		// foreach (string webBpListUrl in Playlists.BpListUrls.Values)
-		// {
-		// 	Task<BPList> t = BeatSaverDownloadManager.GetWebBpListAsync(webBpListUrl);
-		// 	tasks.Add(t);
-		// }
-		//
+		
+		
+		Stopwatch stopwatch = Stopwatch.StartNew();
+		List<Task<PlaylistDetail?>> tasks = new(Playlists.BeatSaverPlaylists.Count + Playlists.SpotifyPlaylistUrls.Count);
+
+		BeatSaver beatSaver = new BeatSaver(nameof(BeatSaberLibraryManager), new System.Version(0, 1));
+		
+		 foreach (int id in Playlists.BeatSaverPlaylists.Values)
+		 {
+		 	Task<PlaylistDetail?> t = beatSaver.Playlist(id);
+		 	tasks.Add(t);
+		 }
+		 
+		 // foreach (int id in Playlists.BeatSaverMapperPlaylists.Values)
+		 // {
+			//  Task<PlaylistDetail?> t = beatSaver.(id);
+			//  tasks.Add(t);
+		 // }
+		
 		// foreach (string spotifyPlaylistUrl in Playlists.SpotifyPlaylistUrls.Values)
 		// {
 		// 	Task<BPList> t = BeatSaverDownloadManager.GenerateBpListFromSpotifyPlaylistAsync(spotifyPlaylistUrl);
@@ -25,6 +37,15 @@ public class Program
 		// }
 		
 		Task.WaitAll(tasks.Cast<Task>().ToArray());
+
+		foreach (Task<PlaylistDetail?> task in tasks)
+		{
+			if (task.Result != null)
+			{
+				Console.WriteLine(task.Result.Playlist.Name);
+			}
+		}
+		
 		//todo: export playlists 
 		Console.WriteLine("All tasks complete in " + stopwatch.ElapsedMilliseconds / 1000f + " seconds");
 		
