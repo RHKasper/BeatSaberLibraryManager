cf51b720b672ce9b0ace3221dbf76f8a70d3aad4
Robert Kasper <roberthkasper@gmail.com>
2023-12-10 23:46:02 -0500

    Adds map zip retry

--- Program.cs
+++ Program.cs
@@ -124,25 +124,59 @@ public class Program
     private static async Task DownloadZipFiles(HashSet<Beatmap> beatmaps)
     {
         CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
-        List<Task> zipFileDownloadAndUnzipTasks = new();
+        Dictionary<Task, Beatmap> zipFileDownloadAndUnzipTasks = new();
         foreach (Beatmap beatmap in beatmaps)
         {
             Console.WriteLine("Requesting download for " + beatmap.Name);
-            zipFileDownloadAndUnzipTasks.Add(DownloadAndUnzipZipFile(beatmap, cancellationTokenSource.Token));
+            zipFileDownloadAndUnzipTasks.Add(DownloadAndUnzipZipFile(beatmap, cancellationTokenSource.Token), beatmap);
         }
 
         Console.WriteLine("Requested " + beatmaps.Count + " map zip files");
         
-        int secondsWaited = 0;
-        while (zipFileDownloadAndUnzipTasks.Any() && secondsWaited < ZipDownloadTimeoutSeconds)
-        {
-            Console.WriteLine("waiting on " + zipFileDownloadAndUnzipTasks.Count(t => !t.IsCompletedSuccessfully) + " zip download tasks");
-            await Task.Delay(1000);
-            secondsWaited += 1;
-        }
+        // wait ten seconds
+        await Task.Delay(10000);
 
-        //todo: retry downloads that didn't successfully finish?
-        cancellationTokenSource.Cancel();
+        int retryCount = 0;
+        int previousNumberOfWaitingTasks = zipFileDownloadAndUnzipTasks.Count;
+        int secondsSinceNumWaitingTasksChanged = 0;
+        
+        // whenever the number of waiting maps remains unchanged for 10 seconds, restart the tasks that haven't resolved. 
+        while (zipFileDownloadAndUnzipTasks.Any() && retryCount < 3)
+        {
+            Console.WriteLine("Waiting on " + previousNumberOfWaitingTasks + " map zip downloads");
+            await Task.Delay(1000);
+            
+            foreach (Task task in zipFileDownloadAndUnzipTasks.Keys)
+            {
+                if (task.IsCompletedSuccessfully)
+                {
+                    zipFileDownloadAndUnzipTasks.Remove(task);
+                }
+            }
+
+            if (zipFileDownloadAndUnzipTasks.Count != previousNumberOfWaitingTasks)
+            {
+                previousNumberOfWaitingTasks = zipFileDownloadAndUnzipTasks.Count;
+                secondsSinceNumWaitingTasksChanged = 0;
+            }
+            else
+            {
+                secondsSinceNumWaitingTasksChanged += 1;
+                if (secondsSinceNumWaitingTasksChanged > 3)
+                {
+                    // retry unresolved tasks
+                    retryCount++;
+                    Beatmap[] beatmapsToRetry = zipFileDownloadAndUnzipTasks.Values.ToArray();
+                    zipFileDownloadAndUnzipTasks.Clear();
+                    
+                    foreach (Beatmap beatmap in beatmapsToRetry)
+                    {
+                        Console.WriteLine("Retrying (#" + retryCount + ") download for " + beatmap.Name);
+                        zipFileDownloadAndUnzipTasks.Add(DownloadAndUnzipZipFile(beatmap, cancellationTokenSource.Token), beatmap);
+                    }
+                }
+            }
+        }
     }
 
     private static async Task DownloadAndUnzipZipFile(Beatmap beatmap, CancellationToken cancellationToken)
